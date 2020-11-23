d3c43c44c65e15f0023489fa241f273778972d06
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-11-23 15:54:08 +0100

    roc curve

--- src/trainFeatex.py
+++ src/trainFeatex.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
     fig = plt.figure()
     lw = 2
     plt.plot(fpr, tpr, color='darkorange',
-             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+             lw=lw, label='ROC curve (area = {})'.format(roc_auc))
     plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
