291366134568f5b0c30d002a179979dc5b1bc98e
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-11-24 17:13:48 +0100

    train 250 for all

--- src/trainFeatex.py
+++ src/trainFeatex.py
@@ -99,18 +99,52 @@ def tmp():
 
 if __name__ == '__main__':
     print("... Loading data")
-    data = np.load("./dataAll.npy")
+    train_data = np.load("./train_data.npy")
+    test_data = np.load("./test_data.npy")
 
     print("... Loading Labels")
-    labels = np.load("./labelsAll.npy")
+    train_label = np.load("./train_label.npy")
+    test_label = np.load("./test_label.npy")
 
-    print("... Spliting data")
+    model = lightfeaturesextract.light_featex()
+    optimizer = Adam(lr=1e-6)
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy', tf.keras.metrics.Recall(),
+                                                                            tf.keras.metrics.AUC(),
+                                                                            tf.keras.metrics.Precision()])
 
-    train_data, test_data, train_label, test_label = train_test_split(data, labels, test_size=0.2, random_state=42)
-    np.save("./train_data.npy", train_data)
-    np.save("./train_label.npy", train_label)
-    np.save("./test_data.npy", test_data)
-    np.save("./test_label.npy", test_label)
+    checkpoint = tf.keras.callbacks.ModelCheckpoint("../pretrained_model/featexAll80.h5",
+                                                    monitor='val_accuracy', verbose=1,
+                                                    save_best_only=True, mode='max')
+    csv_logger = CSVLogger("model_history_log.csv", append=True)
+
+    callbacks_list = [checkpoint, csv_logger]
+
+    history = model.fit(train_data, train_label, epochs=80, batch_size=128,
+                        validation_data=(test_data, test_label), callbacks=callbacks_list)
+
+    model.load_weights("../pretrained_model/featexAll80.h5")
+
+    preds = model.predict(test_data, verbose=1)
+    fpr, tpr, _ = roc_curve(test_label, preds)
+    roc_auc = auc(fpr, tpr)
+
+    fig = plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.savefig("./ROC")
+    plt.close(fig)
+
+
+
+
 
 
 
