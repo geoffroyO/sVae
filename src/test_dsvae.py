f0a7c7afe20ec01e9784b620f24b0ae538308bda
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-11-26 17:25:06 +0100

    okok

--- src/test_dsvae.py
+++ src/test_dsvae.py
@@ -3,18 +3,17 @@ import dsvae as ds
 from tensorflow.keras.optimizers import Adam
 
 import numpy as np
-
-
+from tensorflow.python.keras.optimizers import sgd
 
 if __name__ == '__main__':
     dir = '../pretrained_model/featex_test.h5'
     model = ds.load_dsvae(dir)
-    vae_optimizer = Adam(lr=1e-6)
+    vae_optimizer = sgd(lr=1e-6)
     model.compile(vae_optimizer, ds.vaeLoss)
     data = np.load("./spliced.npy")
     train = data[:128]
     print("... Training")
 
-    model.fit(train, epochs=200, batch_size=32)
+    model.fit(train, epochs=2000, batch_size=32)
     model.save_weights("../pretrained_model/model_test.h5")
 
