20901d9751566eef30ab5d93d952a897034a1994
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-11-26 17:19:07 +0100

    okok

--- src/test_dsvae.py
+++ src/test_dsvae.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
     vae_optimizer = Adam(lr=1e-6)
     model.compile(vae_optimizer, ds.vaeLoss)
     data = np.load("./spliced.npy")
-    train, test = data[:int(len(data)*0.7)], data[int(len(data)*0.7):]
+    train, test = data[:int(len(data)*0.5)]
     print("... Training")
 
-    model.fit(train, epochs=20, batch_size=128, validation_data=(test))
+    model.fit(train, epochs=20, batch_size=128)
     model.save_weights("../pretrained_model/model_test.h5")
 
