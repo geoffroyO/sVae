3918fd7df3b57aa95f53405bd01c64918e9703d1
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-11-27 15:40:16 +0100

    test vae

--- src/anodec.py
+++ src/anodec.py
@@ -1,129 +1,187 @@
+import lightfeaturesextract as lf
+
 import tensorflow as tf
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
 from tensorflow.python.keras import Input, Model
-from tensorflow.python.keras.backend import random_normal
-from tensorflow.python.keras.layers import Conv2D, Dropout, Dense, Flatten, \
-    Conv2DTranspose, Reshape, BatchNormalization, LeakyReLU, ReLU
+from tensorflow.python.keras.callbacks import CSVLogger
+from tensorflow.python.keras.layers import Conv2D, Dense, Flatten, \
+    Conv2DTranspose, Reshape, BatchNormalization, LeakyReLU, Dropout, LayerNormalization
 from tensorflow.keras.optimizers import Adam
-from tensorflow.python.ops.losses.losses_impl import absolute_difference, Reduction
 
 import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.python.ops.losses.losses_impl import absolute_difference, Reduction
 
+
+class Sampling(tf.keras.layers.Layer):
+    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+def discriminator():
+    discriminator_inputs = Input(shape=(32, 32, 128))
+
+    x = Conv2D(256, 5, strides=2, padding="same")(discriminator_inputs)
+    x = LayerNormalization([1, 2])(x)
+    x = LeakyReLU()(x)
+
+    x = Conv2D(512, 5, activation="relu", strides=2, padding="same")(x)
+    x = LayerNormalization([1, 2])(x)
+    x = LeakyReLU()(x)
+
+    x = Conv2D(512, 1, padding='same')(x)
+
+    discriminator_outputs = Dense(1)(x)
+
+    discriminator = Model(discriminator_inputs, discriminator_outputs, name="discriminator")
+
+    return discriminator
 def encoder():
-    latent_dim = 2
+    latent_dim = 128
+    encoder_inputs = Input(shape=(32, 32, 128))
 
-    features_input = Input(shape=(28, 28, 1), name='features_in')
-    encoder = Conv2D(32, 5, strides=2, padding="same")(features_input)
-    encoder = BatchNormalization()(encoder)
-    encoder = LeakyReLU()(encoder)
+    x = Conv2D(256, 5, strides=2, padding="same")(encoder_inputs)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
-    encoder = Conv2D(64, 5, strides=2, padding="same")(encoder)
-    encoder = BatchNormalization()(encoder)
-    encoder = LeakyReLU()(encoder)
+    x = Conv2D(512, 5, activation="relu", strides=2, padding="same")(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
-    encoder = Conv2D(64, 1, padding='same')(encoder)
+    x = Conv2D(512, 1, padding='same')(x)
 
-    encoder = Flatten()(encoder)
+    x = Flatten()(x)
 
-    z_mu = Dropout(0.25)(Dense(latent_dim, name="z_mu")(encoder))
-    z_log_sigma = Dropout(0.25)(Dense(latent_dim, name="z_log_sigma")(encoder))
+    z_mean = Dropout(0.25)(Dense(latent_dim, name="z_mean")(x))
+    z_log_var = Dropout(0.25)(Dense(latent_dim, name="z_log_var")(x))
 
-    z_sigma = tf.exp(z_log_sigma)
-    z_vae = z_mu + random_normal(tf.shape(z_sigma)) * z_sigma
-    encoder = Model(features_input, [z_mu, z_log_sigma, z_sigma, z_vae], name="encoder")
+    z = Sampling()([z_mean, z_log_var])
 
+    encoder = Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
     return encoder
 
 
 def decoder():
-    latent_dim = 2
-    decoder_input = Input(shape=(latent_dim,))
+    latent_inputs = keras.Input(shape=(128,))
+    x = Dropout(0.25)(Dense(8 * 8 * 512)(latent_inputs))
+    x = Reshape((8, 8, 512))(x)
+    x = Conv2D(512, 1, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
-    decoder = Dropout(0.25)(Dense(7 * 7 * 64, activation="relu")(decoder_input))
-    decoder = Reshape((7, 7, 64))(decoder)
-    decoder = Conv2D(512, 1, padding='same')(decoder)
+    x = Conv2DTranspose(512, 3, strides=2, padding="same")(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
-    decoder = BatchNormalization()(decoder)
-    decoder = ReLU()(decoder)
+    x = Conv2DTranspose(256, 3, strides=2, padding="same")(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
-    decoder = Conv2DTranspose(64, 2, strides=2, padding="same")(decoder)
-    decoder = BatchNormalization()(decoder)
-    decoder = LeakyReLU()(decoder)
+    x = Conv2DTranspose(128, 3, padding="same")(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
 
-    decoder = Conv2DTranspose(32, 2, strides=2, padding="same")(decoder)
-    decoder = BatchNormalization()(decoder)
-    decoder = LeakyReLU()(decoder)
+    decoder_outputs = Conv2D(128, 1, strides=1,  padding='same', activation='sigmoid')(x)
 
-    decoder = Conv2DTranspose(1, 5, activation='sigmoid', strides=1, padding='same')(decoder)
-    decoder = Conv2D(1, 1, strides=1, padding='same', name='dec_conv2D_final', activation=tf.identity)(decoder)
-    decoder = Model(decoder_input, decoder, name='decoder')
-
+    decoder = Model(latent_inputs, decoder_outputs, name="decoder")
     return decoder
 
 
-def vaeLoss(features, z_mu, z_sigma, reconstruction):
-    kl_loss = tf.reduce_mean(0.5 * tf.reduce_sum(tf.math.square(z_mu) +
-                                                 tf.math.square(z_sigma) - tf.math.log(tf.square(z_sigma)) - 1,
-                                                 axis=1))
-    L1 = absolute_difference(features, reconstruction, reduction=Reduction.NONE)
-    reconstrution_loss = tf.reduce_mean(tf.reduce_sum(L1, axis=[1, 2, 3]))
-    return reconstrution_loss, kl_loss
-
-
-class svae(Model):
-    def __init__(self):
-        super(svae, self).__init__()
-        self.encoder = encoder()
-        self.decoder = decoder()
-
-    def call(self, inputs):
-        z_mean, z_log_sigma, z_sigma, z_vae = self.encoder(inputs)
-        return self.decoder(z_vae)
-
-    def compile(self, optimizer, loss, **kwargs):
-        super(svae, self).compile()
-        self.svae_optimizer = optimizer
-        self.svaeLoss = loss
+class VAE(keras.Model):
+    def __init__(self, featex, encoder, decoder, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.featex = featex
+        self.encoder = encoder
+        self.decoder = decoder
 
     def train_step(self, data):
         if isinstance(data, tuple):
             data = data[0]
-
         with tf.GradientTape() as tape:
-            z_mu, z_log_sigma, z_sigma, z = self.encoder(data)
+            features = self.featex(data)
+            z_mean, z_log_var, z = self.encoder(features)
             reconstruction = self.decoder(z)
-            reconstrution_loss, kl_loss = self.svaeLoss(data, z_mu, z_sigma, reconstruction)
-            vae_loss = reconstrution_loss + kl_loss
-        grads = tape.gradient(vae_loss, self.trainable_weights)
-        self.svae_optimizer.apply_gradients(
-            zip(grads, self.trainable_weights)
-        )
+            L1 = absolute_difference(features, reconstruction, reduction=Reduction.NONE)
+            reconstruction_loss = tf.reduce_mean(tf.reduce_sum(L1, axis=[1, 2, 3]))
+            kl_loss = 1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
+            kl_loss = tf.reduce_mean(kl_loss)
+            kl_loss *= -0.5
+            total_loss = reconstruction_loss + kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
         return {
-            "total_loss": vae_loss,
-            "reconstruction_loss": reconstrution_loss,
+            "loss": total_loss,
+            "reconstruction_loss": reconstruction_loss,
             "kl_loss": kl_loss,
         }
 
 
 if __name__ == '__main__':
-    (train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
+    data = np.load("./data_to_load/spliced.npy")
+    labels = np.load("./data_to_load/spliced_labels.npy")
 
-    def preprocess_images(images):
-        images = images.reshape((images.shape[0], 28, 28, 1)) / 255.
-        return np.where(images > .5, 1.0, 0.0).astype('float32')
+    train_data, test_data, train_label, test_label = train_test_split(data, labels, test_size=0.2, random_state=42)
 
-
-    train_images = preprocess_images(train_images)
-    test_images = preprocess_images(test_images)
-
-    train_size = 60000
-    batch_size = 32
-    test_size = 10000
-
-    train_dataset = (tf.data.Dataset.from_tensor_slices(train_images)
-                     .shuffle(train_size))
-    test_dataset = (tf.data.Dataset.from_tensor_slices(test_images)
-                    .shuffle(test_size))
-    sva = svae()
+    model = lf.light_featex()
     optimizer = Adam(lr=1e-6)
-    sva.compile(optimizer, vaeLoss)
-    sva.fit(train_images, epochs=10, batch_size=32)
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy', tf.keras.metrics.Recall(),
+                                                                            tf.keras.metrics.AUC(),
+                                                                            tf.keras.metrics.Precision()])
+
+    checkpoint = tf.keras.callbacks.ModelCheckpoint("../pretrained_model/featex_spliced_250.h5",
+                                                    monitor='val_accuracy', verbose=1,
+                                                    save_best_only=True, mode='max')
+    csv_logger = CSVLogger("featex_spliced_250.csv", append=True)
+
+    callbacks_list = [checkpoint, csv_logger]
+
+    history = model.fit(train_data, train_label, epochs=250, batch_size=128,
+                        validation_data=(test_data, test_label), callbacks=callbacks_list)
+
+    model.load_weights("../pretrained_model/featex_spliced_250.h5")
+
+    preds = model.predict(test_data, verbose=1)
+    fpr, tpr, _ = roc_curve(test_label, preds)
+    roc_auc = auc(fpr, tpr)
+
+    fig = plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.savefig("./ROC_featex_spliced_250")
+    plt.close(fig)
+
+
+    dir = "../pretrained_model/featex_spliced_250.h5"
+    featex = lf.load_featex(dir)
+    encoder = encoder()
+    decoder = decoder()
+    vae = VAE(featex, encoder, decoder)
+
+    vae.compile(optimizer=Adam(lr=1e-6))
+
+    checkpoint = tf.keras.callbacks.ModelCheckpoint("../pretrained_model/anodec_spliced_100.h5",
+                                                    monitor='val_accuracy', verbose=1,
+                                                    save_best_only=True, mode='max')
+    csv_logger = CSVLogger("anodec_spliced_100.csv", append=True)
+
+    callbacks_list = [checkpoint, csv_logger]
+
+    vae.fit(train_data, epochs=100, batch_size=128, callbacks=callbacks_list)
+
+    vae.load_weights("../pretrained_model/anodec_spliced_100.h5")
+
+
