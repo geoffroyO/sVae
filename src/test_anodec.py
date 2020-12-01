e46bf00dd65de16c730d706cfa8bebc2e662615a
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-12-01 11:26:24 +0100

    load anodec test

--- src/test_anodec.py
+++ src/test_anodec.py
@@ -21,7 +21,7 @@ class postTreat(keras.Model):
         super(postTreat, self).__init__(**kwargs)
         self.anodec = anodec
         self.subtract = Subtract()
-        self.finalConv = Conv2D(1, 3, strides=1, padding='same', name='finalConv')
+        self.finalConv = Conv2D(1, 3, strides=1, padding='same', activation='sigmoid', name='finalConv')
 
     def call(self, input):
         anoFeat = self.anodec(input)
@@ -30,63 +30,33 @@ class postTreat(keras.Model):
         mask = self.finalConv(sub)
         return mask
 
-def load_data():
-    print("... Loading data")
-    spliced, copy_moved, spliced_mask, copy_moved_mask = lm.load_images("../data/CASIA2/Tp/", "../data/CASIA2/gt/")
-    print("... Patching images")
-    data, labels = lm.patch_images(spliced, spliced_mask)
-    print("... Normalizing images")
-    data = [rgb.astype('float32') / 255. for rgb in tqdm(data)]
-    labels2 = []
-    print("... Labelizing")
-    mask = labels
-    for label in tqdm(labels):
-        tp = np.sum(label) / 255
-        percent = tp * 100 / (32 * 32)
-        if 12.5 < percent < 87.5:
-            labels2.append(1)
-        else:
-            labels2.append(0)
-    labels = labels2
-    tt = np.sum(labels)
-    count = 0
-    dataf = []
-    labelsf = []
-    for k, img in tqdm(enumerate(data)):
-        if labels[k] == 0:
-            if count <= tt:
-                count += 1
-                dataf.append(img)
-                labelsf.append(mask[k])
-        if labels[k] == 1:
-            dataf.append(img)
-            labelsf.append(mask[k])
-    data = dataf
-    mask = labelsf
-    tmp = list(zip(data, mask))
-    random.shuffle(tmp)
-    data, labels = zip(*tmp)
-    data = np.array(data)
-    mask = np.array(mask)
-    return data, mask
+
+def dice(img1, img2):
+    inter = 0
+    tt1, tt2 = 0, 0
+    N, M = img1.shape
+    for i in range(N):
+        for j in range(M):
+            ind1, ind2 = img1[i, j], img2[i, j]
+            if ind1:
+                tt1 += 1
+            if ind2:
+                tt2 += 1
+            if ind1 and ind2:
+                inter += 1
+    return 2*inter/(tt1 + tt2)
 
 
 if __name__ == '__main__':
-    data, mask = load_data()
-    np.save("./data_to_load/splicedFinal.npy", data)
-    np.save("./data_to_load/maskSplicedFinal.npy", mask)
-    """
     dirFeatex = "../pretrained_model/featex_spliced_250.h5"
     dirAno = "../pretrained_model/anodec_spliced_250.h5"
     anodec = ano.load_anodec(dirFeatex, dirAno)
 
     model = postTreat(anodec)
-    model.build(input_shape=(None, 32, 32, 3))
-    model.summary()
-    
-    data = np.load("./data_to_load/spliced.npy")
-    labels = np.load("./data_to_load/spliced_labels.npy")
-    mask = np.load("./data_to
+    model.compile(loss=dice, optimizer=Adam(lr=1e-6))
 
-    train_data, test_data, train_label, test_label = train_test_split(data, labels, test_size=0.2, random_state=42)
-    """
+    data = np.load("./data_to_load/splicedFinal.npy")
+    mask = np.load("./data_to_load/maskSplicedFinal.npy")
+
+    train_data, test_data, train_mask, test_mask = train_test_split(data, mask, test_size=0.2, random_state=42)
+    model.fit(train_data, train_mask, epochs=10, validation_data=(test_data, test_mask))
