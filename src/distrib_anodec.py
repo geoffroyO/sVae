f1b73cf35f9766b053075086f8bbc29a4e4003d7
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-12-01 15:43:37 +0100

    load anodec test

--- src/distrib_anodec.py
+++ src/distrib_anodec.py
@@ -7,14 +7,27 @@ import numpy as np
 if __name__ == '__main__':
 
     data = np.load("./data_to_load/splicedFinal.npy")
-    mask = np.load("./data_to_load/maskSplicedFinal.npy")
+    labels = np.load("./data_to_load/spliced_labels.npy")
+    data_tmp = []
+    count_tamp, count_ori = 0, 0
+    k = 0
+    while count_tamp < 3 and count_ori < 3:
+        if labels[k]:
+            count_tamp += 1
+            data_tmp.append(data[k])
+        else:
+            count_ori += 1
+            data_tmp.append(data[k])
+        k += 1
+
+    data = data_tmp
 
     dirFeatex = "../pretrained_model/featex_spliced_250.h5"
     dirAno = "../pretrained_model/anodec_spliced_250.h5"
 
     anodec = ano.load_anodec(dirFeatex, dirAno)
 
-    features_data = anodec.featex.predict(data[:5])
+    features_data = anodec.featex.predict(data)
     print("****{}****".format(features_data.shape))
 
     model = Model(anodec.encoder.input, anodec.encoder.layers[8].output)
