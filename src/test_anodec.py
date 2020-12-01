26e80efd1c1c3d837be911efb1ba11f9be2b93ff
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-12-01 11:54:10 +0100

    load anodec test

--- src/test_anodec.py
+++ src/test_anodec.py
@@ -62,5 +62,5 @@ if __name__ == '__main__':
 
     train_data, test_data, train_mask, test_mask = train_test_split(data, mask, test_size=0.2, random_state=42)
     pred = model.predict(np.array([train_data[0]]))
-    print("*****{}*****".format(pred.shape))
+    print(pred)
     #model.fit(train_data, train_mask, epochs=10, validation_data=(test_data, test_mask), batch_size=128)
