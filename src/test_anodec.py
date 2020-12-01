0b1d2558d5840c45315cb3039b579505bcd8679f
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-12-01 10:17:50 +0100

    load anodec test

--- src/test_anodec.py
+++ src/test_anodec.py
@@ -3,4 +3,5 @@ if __name__=='__main__':
     dirFeatex = "../pretrained_model/featex_spliced_250.h5"
     dirAno = "../pretrained_model/anodec_spliced_250.h5"
     model = ano.load_anodec(dirFeatex, dirAno)
+    model.trainable = False
     model.summary()
