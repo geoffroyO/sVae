b39731ef1c46afb0de14ee0e0f7ec97b1f3721c1
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-12-07 11:46:17 +0100

    test new models

--- src/test_end.py
+++ src/test_end.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     img = img.astype('float32') / 255.
 
     res = model.predict(np.array([img[0:+32, 0:32], img[0:+32, 0:32], img[0:+32, 0:32]]))
-    print("*****{}*****".format(res.shape))
+    print(res[0,:,:,0])
     """
     for k in tqdm(range(1, 7)):
         path = "./img_test/{}.jpg".format(k)
