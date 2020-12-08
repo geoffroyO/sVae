47315392ec8f0d10b4f27d1655d2798f8f2e3b31
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-12-08 11:53:13 +0100

    test

--- src/test_end.py
+++ src/test_end.py
@@ -50,19 +50,15 @@ def test_all():
         img = img.astype('float32') / 255.
 
         mask = pred(model, img, 32)
-        n, m = mask.shape
-        for i in range(n):
-            for j in range(m):
-                if mask[i, j] > 0.99:
-                    mask[i, j] = 255
-                else:
-                    mask[i, j] = 0
-        figure = plt.figure()
-        """        
+        np.save("./img_test/{}.npy".format(k), mask)
+
+        """
+        figure = plt.figure()   
         sn.heatmap(mask, cmap="YlGnBu", center=np.mean(mask))
-        plt.savefig("./img_test/{}_pred_gt.jgp".format(k), format='jpg')"""
+        plt.savefig("./img_test/{}_pred_gt.jgp".format(k), format='jpg')
         plt.imsave("./img_test/{}_pred_gt.jpg".format(k), arr=mask, format='jpg')
         plt.close(figure)
+        """
 
 
 def test_featex():
