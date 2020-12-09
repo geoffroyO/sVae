e54f0bd39de9ed88a4139acc23d918eb2dd9c6da
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-12-09 16:24:52 +0100

    test

--- src/discriminativeVae.py
+++ src/discriminativeVae.py
@@ -120,9 +120,9 @@ def otsu(error):
 
     for eps in np.arange(0, 1.01, 0.01):
         cond1 = tf.where(error >= eps, error, tf.zeros_like(error))
-        mean1 = tf.reduce_mean(cond1, axis = [1, 2])
+        mean1 = tf.reduce_mean(cond1, axis=[1, 2])
         count1 = tf.where(error >= eps, tf.zeros_like(error)+1, tf.zeros_like(error))
-        count1 = tf.reduce_sum(count1)
+        count1 = tf.reduce_sum(count1, axis=[1, 2])
         print("***************")
         print(mean1)
         print(count1)
