7d4c191206f5d698e1f92d1ad3965ccd8fe95ed3
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-12-11 10:06:15 +0100

    test

--- src/eagerDiscriminativeVae.py
+++ src/eagerDiscriminativeVae.py
@@ -205,7 +205,7 @@ class disciminativeAno(keras.Model):
 
             reconstruction_loss = dicriminative_error(error, threshold)
             reconstruction_loss = tf.reduce_mean(reconstruction_loss)
-            print("******{}*****".format(reconstruction_loss.numpy()))
+            print("******{}*****".format(tf.cast(reconstruction_loss, dtype=tf.float32)))
 
             kl_loss = -0.5*tf.reduce_mean(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
             print("******{}*****".format(kl_loss.astype("float32")))
