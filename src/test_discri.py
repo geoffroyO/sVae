20b586da3b19fac81dc0f2c705c003c00154c025
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2020-12-16 12:32:17 +0100

    ok

--- src/test_discri.py
+++ src/test_discri.py
@@ -92,7 +92,7 @@ def preddiscrVae(model, img, block_size):
 
 
 if __name__=='__main__':
-    pathModel = "../models/discriminativeAno_30.h5"
+    pathModel = "../models/noSRMdiscriminativeAno_50.h5"
 
     encoder = dv.encoder()
     decoder = dv.decoder()
