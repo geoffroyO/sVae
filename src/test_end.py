065173d805160060360720149469688896a07a17
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2021-01-11 15:13:25 +0100

    test

--- src/test_end.py
+++ src/test_end.py
@@ -145,6 +145,48 @@ def test_endVae():
         np.save("./img_test/{}_error.npy".format(k), error)
 
 
+def enumMatrix4K(N, M, block_size):
+    enum = np.zeros((N, M))
+    for i in range(0, N-block_size+1, 40):
+        for j in range(0, M-block_size+1, 40):
+            enum[i:(i+block_size), j:(j+block_size)] += 1
+    return enum
+
+
+def predendVae4K(model, img, block_size):
+    N, M, C = img.shape
+    reconstuction_img, features_img, mask_error = np.zeros((N, M, C)), np.zeros((N, M, C)), np.zeros((N, M))
+
+    blocks = []
+    print("... Creating blocks")
+    for i in tqdm(range(0, N-block_size+1, 40)):
+        for j in range(0, M-block_size+1, 40):
+            blocks.append(img[i:(i+block_size), j:(j+block_size)])
+
+    blocks = np.array(blocks)
+    features, reconstruction, error = model.predict(blocks)
+    count = 0
+    print("... Prediction for each blocks")
+    for i in tqdm(range(0, N-block_size+1, 40)):
+        for j in range(0, M-block_size+1, 40):
+            mask_error_pred = error[count]
+            mask_error[i:(i+block_size), j:(j+block_size)] += mask_error_pred
+
+            block_reconstruction = reconstruction[count]
+            reconstuction_img[i:(i+block_size), j:(j+block_size)] += block_reconstruction
+
+            block_features = features[count]
+            features_img[i:(i+block_size), j:(j+block_size)] += block_features
+            count += 1
+    enum = enumMatrix4K(N, M, block_size)
+    mask_error /= enum
+    enum_3D = np.dstack((enum, enum))
+    enum_3D = np.dstack((enum_3D, enum))
+    reconstuction_img /= enum_3D
+    features_img /= enum_3D
+    return reconstuction_img, features_img, mask_error
+
+
 def test_endVae4K():
     pathModel = "../models/srmBlurredEndAno4K_250.h5"
 
