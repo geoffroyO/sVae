4226216848a476b23925f845c8cac4d47e28032e
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2021-01-13 12:14:12 +0100

    ok

--- src/test_end.py
+++ src/test_end.py
@@ -196,7 +196,7 @@ def test_endVae4K():
 
     model.load_weights(pathModel)
 
-    for k in range(1, 8):
+    for k in range(1, 10):
         path = "./4K_cmb/{}.png".format(k)
 
         img = cv2.imread(path, 1)
